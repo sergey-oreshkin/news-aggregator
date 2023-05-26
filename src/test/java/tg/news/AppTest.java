45b2b63d1ccaadeb85cbcd32b0fbeff449cb1511
sergey-oreshkin <joknife79@yandex.ru>
2023-05-26 11:23:28 +0500

    add test

---
+++ src/test/java/tg/news/AppTest.java
@@ -0,0 +1,10 @@
+package tg.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+    @Test
+    void contextLoad(){}
+}
